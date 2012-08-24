2387e10dc37021a85d647082986e33b8c187cb3b
rallus <rallus@rallus-pc>
2012-08-24 16:31:43 +0200

    Add menu bar with basics actions

--- src/org/Jalgo/model/JWindow.java
+++ src/org/Jalgo/model/JWindow.java
@@ -3,6 +3,9 @@ package org.Jalgo.model;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
@@ -17,9 +20,54 @@ public class JWindow extends JFrame{
 		setContentPane(windowPanel);
 	}
 	
+	public void createMenuBar(){
+		JMenuBar windowMenuBar;
+		JMenu windowMenu;
+		JMenuItem windowMenuItem;
+		
+		windowMenuBar = new JMenuBar();
+		
+		windowMenu = new JMenu("Fichier");
+		windowMenuBar.add(windowMenu);
+		
+		windowMenuItem = new JMenuItem("Nouveau Projet");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Nouvelle fonction");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Importer un Projet");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Sauvegarder");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Fermer");
+		windowMenu.add(windowMenuItem);
+		
+		windowMenu = new JMenu("Actions");
+		windowMenuBar.add(windowMenu);
+		
+		windowMenuItem = new JMenuItem("Nouvelle opération");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Nouvelle condition");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Nouvelle Boucle");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("Appel fonction");
+		windowMenu.add(windowMenuItem);
+		
+		windowMenu = new JMenu("Aide");
+		windowMenuBar.add(windowMenu);
+		
+		windowMenuItem = new JMenuItem("Documentation");
+		windowMenu.add(windowMenuItem);
+		windowMenuItem = new JMenuItem("A propos");
+		windowMenu.add(windowMenuItem);
+		
+		this.setJMenuBar(windowMenuBar);
+	}
+	
 	public static void main(String args[]){
 		JWindow mainWindow = new JWindow();
 		
+		mainWindow.createMenuBar();
 		mainWindow.pack();
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setVisible(true);
